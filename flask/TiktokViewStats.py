98379fc9d4e00e5abedbdf7127c1519bc9284cdf
Dewi Sekarini <dewi.sekarini@btpn.com>
2023-03-02 21:15:00 +0700

    log in tiktok view

--- flask/TiktokViewStats.py
+++ flask/TiktokViewStats.py
@@ -86,7 +86,8 @@ class livecounts:
         )
     
     @staticmethod
-    def getIdFromLongUrl(url: str) -> dict:       
+    def getIdFromLongUrl(url: str) -> dict:
+        print("this is short url", url)       
         headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
         }
