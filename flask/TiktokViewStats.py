1a0c5cd3b7c5afb43095548272c07801e364a36a
Dewi Sekarini <dewi.sekarini@btpn.com>
2023-03-25 23:24:31 +0700

    pake acc

--- flask/TiktokViewStats.py
+++ flask/TiktokViewStats.py
@@ -44,6 +44,7 @@ class livecounts:
             'authority' : 'tiktok.livecounts.io',
             'origin'    : 'https://livecounts.io',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
         }
         print("5")
         #proxies = livecounts.__getProxies()
